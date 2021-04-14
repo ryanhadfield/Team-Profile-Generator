fc20582c9cc469ec863387f7363f4fbb88504e57
Ryan Hadfield <ryanhadfield@gmail.com>
2021-04-14 14:43:02 -0600

    Update Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,18 +3,18 @@ const Engineer = require("../lib/Engineer");
 describe("testing engineer constructor", () => {
     describe("test engineer properties", () => {
         test("test github property", () => {
-            const engineer = new Engineer("Ron Burgundy", 1, "rburgundy@channel4news.com", "RonBurgundy");
-            expect(engineer.github).toEqual("RonBurgundy");
+            const engineer = new Engineer("Brian Fantana", 1, "bfantana@channel4news.com", "BrianFantana");
+            expect(engineer.github).toEqual("BrianFantana");
         });
     });
 
     describe("test engineer methods", () => {
         test("test getGitHub method", () => {
-            const engineer = new Engineer("Ron Burgundy", 1, "rburgundy@channel4news.com", "RonBurgundy");
-            expect(engineer.getGitHub()).toEqual("RonBurgundy");
+            const engineer = new Engineer("Brian Fantana", 1, "bfantana@channel4news.com", "BrianFantana");
+            expect(engineer.getGitHub()).toEqual("BrianFantana");
         });
         test("test getRole method", () => {
-            const engineer = new Engineer("Ron Burgundy", 1, "rburgundy@channel4news.com", "Engineer");
+            const engineer = new Engineer("Brian Fantana", 1, "bfantana@channel4news.com", "Engineer");
             expect(engineer.getRole()).toEqual("Engineer");
         });
     });
